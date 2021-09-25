6b228dc061f797f1ceaf6f84b4d54bcfa20c7d15
Huy Giang <gnhuy91@gmail.com>
2021-09-25 19:39:37 +0700

    add clj-kondo config & update deps

--- dist/client/about.9A007C4C51AF256FB62D0AAABF519611.js
+++
@@ -1,3 +0,0 @@
-(function(){
-'use strict';$APP.si=function(a){$APP.Xm(a);$APP.t(!1)&&$APP.t(Sn)&&(Sn.H?Sn.H():Sn.call(null));return $APP.Pn.createElement($APP.fi,null,$APP.Pn.createElement("h1",null,"About page"))};$APP.ak("about");if($APP.t(!1))var Sn=$APP.Zm();$APP.t(!1)&&($APP.t(Sn)&&(Sn.D?Sn.D($APP.si,"",null,null):Sn.call(null,$APP.si,"",null,null)),$APP.Ym($APP.si,"app.pages.about/page"));$APP.Zj.$c();
-}).call(this);
