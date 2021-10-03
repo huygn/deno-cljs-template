1039c8857376f9f519fb8da7c9049ff360991ba4
Huy Giang <gnhuy91@gmail.com>
2021-10-03 11:17:04 +0700

    sablono & hiccup syntax
    This reverts commit 28f3a99526b2d159c91386e1120288ea6a4920ca.

--- src/app/lib.clj
+++ src/app/lib.clj
@@ -1,5 +1,6 @@
 (ns app.lib
-  (:require [helix.core :as helix]))
+  (:require [helix.core :as helix]
+            [sablono.core]))
 
 (def features {:fast-refresh true
                :check-invalid-hooks-usage true})
@@ -18,3 +19,19 @@
        ;; flags in special cases
        ~(merge default-opts opts)
        ~@body)))
+
+(defmacro defc [type params & body]
+  (let [[docstring params body] (if (string? params)
+                                  [params (first body) (rest body)]
+                                  [nil params body])
+        opts? (map? (first body)) ;; whether an opts map was passed in
+        opts (if opts? (first body) {})
+        body (if opts? (rest body) body)
+        ;; feature flags to enable by default
+        default-opts {:helix/features features}]
+    `(helix/defnc ~type ~@(when docstring [docstring]) ~params
+       ;; we use `merge` here to allow indidivual consumers to override feature
+       ;; flags in special cases
+       ~(merge default-opts opts)
+       ;; hiccup parsing
+       (sablono.core/html (do ~@body)))))
